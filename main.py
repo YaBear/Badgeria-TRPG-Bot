ca7af748d3a0e91131cc4d0b5b0abf87e1e6bd45
Bradamar Longbottom - Владислав Пугачев <bradamar@student.21-school.ru>
2023-09-05 01:32:58 +0300

    new: start work

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from config_reader import config
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value())
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
